b263fc2e93870198023a5e50fff168cbcc481be0
Tara Ava <ursula.tara.ava@gmail.com>
2021-12-23 10:07:19 +0700

    My First Commit

---
+++ issue.dropdown.js
@@ -0,0 +1,5 @@
+function issueactive() {
+    document.getElementById('issue').class='class="nav-link dropdown-toggle active"';
+    document.getElementById('home').class='class="nav-link"';
+    document.getElementById('resources').class='class="nav-link"';
+}
