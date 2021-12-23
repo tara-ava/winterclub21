f2e05ea1bdf68ca6007b1d0dfa94429a947e344f
Tara Ava <ursula.tara.ava@gmail.com>
2021-12-23 23:11:05 +0700

    added carousel and navbar links, changed color palatte

--- issue.dropdown.js
+++
@@ -1,5 +0,0 @@
-function issueactive() {
-    document.getElementById('issue').class='class="nav-link dropdown-toggle active"';
-    document.getElementById('home').class='class="nav-link"';
-    document.getElementById('resources').class='class="nav-link"';
-}
